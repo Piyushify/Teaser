68950b9a6f8a28947e1598a817a4ffe4a6c95ef9
Piyushify <piyushsharma3042007@gmail.com>
2026-01-27 19:04:32 +0530

    update

--- main.js
+++ main.js
@@ -1,11 +1,1 @@
 console.log('JS Connected');
-function moveRight(){
-  car = document.getElementById("car");
-  current = parseInt(car.style.left) || 0;
-  car.style.left = current + 100 + "px";
-}
-function moveLeft(){
-  car = document.getElementById("car");
-  current = parseInt(car.style.left) || 0;
-  car.style.left = current - 100 + "px";
-}
